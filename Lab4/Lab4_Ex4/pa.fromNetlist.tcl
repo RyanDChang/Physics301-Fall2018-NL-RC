cbc623962aeae69abc37450944837ebb5806e48c
Ryan Chang <32244713+RyanDChang@users.noreply.github.com>
2018-09-23 18:39:15 -0700

    Lab 4 - Complete
    Experiments 3, 4, and 5 are completed and are ready to upload.

--- Lab4/Lab4_Ex4/pa.fromNetlist.tcl
+++ Lab4/Lab4_Ex4/pa.fromNetlist.tcl
@@ -1,7 +1,7 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name Lab4_Ex4 -dir "C:/Users/Vason/Documents/GitHub/Physics301-Fall2018-NL-RC/Lab4/Lab4_Ex4/planAhead_run_1" -part xc3s100ecp132-4
+create_project -name Lab4_Ex4 -dir "C:/Users/Vason/Documents/GitHub/Physics301-Fall2018-NL-RC/Lab4/Lab4_Ex4/planAhead_run_2" -part xc3s100ecp132-4
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
 set_property edif_top_file "C:/Users/Vason/Documents/GitHub/Physics301-Fall2018-NL-RC/Lab4/Lab4_Ex4/Lab4_ex4.ngc" [ get_property srcset [ current_run ] ]
 add_files -norecurse { {C:/Users/Vason/Documents/GitHub/Physics301-Fall2018-NL-RC/Lab4/Lab4_Ex4} }
